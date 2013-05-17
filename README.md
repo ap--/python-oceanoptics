47da976f73eae8be85a76756f6ae55dd2962fcb1
ap-- <mail@andreaspoehlmann.de>
2013-05-17 16:23:52 +0200

    get documents, updated README

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ Currently working with:
 * USB2000+
 
 Contributions are welcome.
+
+
+#### Possible Additions
+
+* STS
+* USB2000
+* USB-LS450
+* USB-ISS-UVVIS
+* USB4000
+* HR2000
+* HR2000+
+* HR4000
+* Jaz
+* apex
+* maya
+* maya2000pro
+* QE65pro
+* QE65000 
+* NIR 
+* NIRQUEST
+* Torus
+
---
+++ docs/download_docs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20USB2000+.pdf
+wget http://www.oceanoptics.com/technical/engineering/STS%20Data%20Sheet.pdf
+wget http://www.oceanoptics.com/technical/engineering/USB2000%20OEM%20Data%20Sheet.pdf
+wget http://www.oceanoptics.com/technical/engineering/USB4000%20OEM%20Data%20Sheet.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20HR2000.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20HR2000+.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20HR4000.pdf
+wget http://www.oceanoptics.com/technical/engineering/Jaz%20OEM%20Data%20Sheet.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20Apex.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20Maya.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20Maya2000Pro\(v3_00_1\).pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20QE65000.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20QE65Pro.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20NIR.pdf
+wget http://www.oceanoptics.com/technical/engineering/OEM%20Data%20Sheet%20--%20NIRQuest.pdf
+wget http://www.oceanoptics.com/technical/engineering/Torus%20OEM%20Data%20Sheet.pdf
