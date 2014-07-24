41fe82ce04c1d70317aaccb9d68c3ef5e9c424c8
ap-- <mail@andreaspoehlmann.de>
2014-07-24 11:52:30 +0200

    Fixed setup.py and import errors.

--- oceanoptics/__init__.py
+++ oceanoptics/__init__.py
@@ -1,17 +1,19 @@
 
-from spectrometers.USB2000 import USB2000
-from spectrometers.USB2000plus import USB2000plus
-from spectrometers.USB4000 import USB4000
-from spectrometers.HR2000plus import HR2000plus
-from spectrometers.HR4000 import HR4000
-from spectrometers.MAYA import MAYA
-from spectrometers.MAYA2000pro import MAYA2000pro
-from spectrometers.APEX import APEX
-from spectrometers.QE65000 import QE65000
-from spectrometers.QE65pro import QE65pro
-from spectrometers.TORUS import TORUS
-from spectrometers.STS import STS
+import oceanoptics.spectrometers
 
-from defines import OceanOpticsError
+from oceanoptics.spectrometers.USB2000 import USB2000
+from oceanoptics.spectrometers.USB2000plus import USB2000plus
+from oceanoptics.spectrometers.USB4000 import USB4000
+from oceanoptics.spectrometers.HR2000plus import HR2000plus
+from oceanoptics.spectrometers.HR4000 import HR4000
+from oceanoptics.spectrometers.MAYA import MAYA
+from oceanoptics.spectrometers.MAYA2000pro import MAYA2000pro
+from oceanoptics.spectrometers.APEX import APEX
+from oceanoptics.spectrometers.QE65000 import QE65000
+from oceanoptics.spectrometers.QE65pro import QE65pro
+from oceanoptics.spectrometers.TORUS import TORUS
+from oceanoptics.spectrometers.STS import STS
 
-from utils import get_a_random_spectrometer
+from oceanoptics.defines import OceanOpticsError
+
+from oceanoptics.utils import get_a_random_spectrometer
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='oceanoptics',
-    version='0.2.0',
+    version='0.2.1',
     author='Andreas Poehlmann, Jose A. Jimenez-Berni, Ben Gamari',
     author_email='mail@andreaspoehlmann.de',
-    packages=['oceanoptics'],
+    packages=['oceanoptics', 'oceanoptics.spectrometers'],
     description='A Python driver for Ocean Optics spectrometers.',
     long_description=open('README.md').read(),
     requires=['python (>= 2.7)', 'pyusb (>= 1.0)', 'numpy'],
