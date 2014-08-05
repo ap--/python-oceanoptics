1aa5c4342d4a049d75ae77d62c0b3159fdfbbe33
ap-- <mail@andreaspoehlmann.de>
2014-08-05 23:56:05 +0200

    Fix wrong time unit value in init

--- oceanoptics/base.py
+++ oceanoptics/base.py
@@ -228,7 +228,7 @@ class OceanOpticsBase(OceanOpticsSpectrometer, OceanOpticsUSBComm):
         return status
 
     def _init_robust_spectrum(self):
-        self.integration_time(1000)
+        self.integration_time(0.001)
         for i in range(10):
             try:
                 self._request_spectrum()
